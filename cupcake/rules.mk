33e808d3868c3d2f721d27a43a13611eb0f16ae5
Naso <43310384+nnaa0504@users.noreply.github.com>
2022-07-17 14:44:52 +0900

    Add files via upload

--- cupcake/rules.mk
+++ cupcake/rules.mk
@@ -61,6 +61,7 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
+VIA_ENABLE = yes
 BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
